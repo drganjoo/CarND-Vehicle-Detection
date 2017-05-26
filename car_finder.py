a72e2e637e487f8f0eaa2e5a6f89fa020673dd17
Fahad Zubair <fahadzubair@gmail.com>
2017-05-26 22:45:13 +0500

    project video added

--- car_finder.py
+++ car_finder.py
@@ -167,7 +167,7 @@ RIGHT_THRESHOLD = 10
 LEFT_THRESHOLD = 20
 MAX_MISSFRAMES = 10
 MIN_FRAMES_TO_CENTER_SEARCH = 5
-FRAME_COUNT_MAYBE_CARS = 10
+FRAME_COUNT_MAYBE_CARS = 8
 MAX_FRAME_COUNT_MAYBE_CARS = 10
 
 class Vehicle():
@@ -183,9 +183,18 @@ class Vehicle():
         self.miss_frames = 0
         self.accounted_miss_frames = 0
         self.frame_no_when_seen = 0 # which frame did we see this car
+        
 
-    def is_box_enclosing(self, box):
-        return (self.center[0] >= box[0][0]) and (self.center[0] <= box[1][0])                 and (self.center[1] >= box[0][1]) and (self.center[1] <= box[1][1])
+    def is_inside_box(self, box):
+        #return (self.center[0] >= box[0][0]) and (self.center[0] <= box[1][0]) and (self.center[1] >= box[0][1]) and (self.center[1] <= box[1][1])
+        car_mask = np.zeros(shape=(720,1280))
+        box_mask = np.zeros(shape=(720,1280))
+
+        box_mask[box[0][1]:box[1][1], box[0][0]:box[1][0]] = True
+        cb = self.bounding_box
+        car_mask[cb[0][1]:cb[1][1], cb[0][0]:cb[1][0]] = True
+
+        return (np.any(np.logical_and(box_mask, car_mask)))
     
     def might_go_out(self):
         # in case the car is towards the bottom of the screen and is not a new car
@@ -252,9 +261,9 @@ class VehicleIdentifier():
         self.min_frames = 20
         self.frames_to_avg = 10
         self.heatmaps = deque(maxlen = self.frames_to_avg)
-        self.cars = []
-        self.cars_gone_off = []
-        self.maybe_cars = []
+        self.cars = set()
+        self.cars_gone_off = set()
+        self.maybe_cars = set()
         self.last_frame_no = 0  # helps in debugging, pickle object and then start again from same frame
         self.car_finder = SingleFrameCarFinder(svc, X_scaler, params_for_feature)
         self.car_windows = None
@@ -267,7 +276,6 @@ class VehicleIdentifier():
         
     def get_bounding_box(self, labels, car_no):
         mask = labels[0] == car_no
-
         nonzero = (mask).nonzero()
 
         # Identify x and y values of those pixels
@@ -278,19 +286,16 @@ class VehicleIdentifier():
         return bbox
 
     def get_car_for_bbox(self, bbox, which_cars = None):
-        car = None
-        center = get_center(bbox)
-        
         if which_cars is None:
             which_cars = self.cars
 
+        cars = set()
         for c in which_cars:
-            #if c.is_within_bounds(center):
-            if (c.center[0] >= bbox[0][0]) and (c.center[0] <= bbox[1][0]) and (c.center[1] >= bbox[0][1]) and (c.center[1] <= bbox[1][1]):
-                car = c
-                break
+            #if (c.center[0] >= bbox[0][0]) and (c.center[0] <= bbox[1][0]) and (c.center[1] >= bbox[0][1]) and (c.center[1] <= bbox[1][1]):
+            if c.is_inside_box(bbox):
+                cars.add(c)
                 
-        return car
+        return cars
     
     def draw_labels_img(self, labels):
         labels_img = np.zeros_like(self.img_rgb)
@@ -340,12 +345,8 @@ class VehicleIdentifier():
         
         self.labels = label(self.combined_heatmap)
         
-        # create a dictionary for all cars we know off
-        # check which ones are not here in this frame and mark their miss_frame count
-        cars_seen_this_frame = {}
-
-        for car in self.cars:
-            cars_seen_this_frame[car] = False
+        # create a set that will hold all cars we saw in this frame
+        cars_seen_this_frame = set()
 
         # gather all car boxes for this frame
         car_boxes = []
@@ -353,124 +354,37 @@ class VehicleIdentifier():
             car_boxes.append(self.get_bounding_box(self.labels, car_no))
 
         # identify new cars and remove old ones
+
+
         for car_box in car_boxes:
-            car = self.get_car_for_bbox(car_box)
+            cars_in_box = self.get_car_for_bbox(car_box)
             
-            if car is None:
-                print('There is a possible car at box: {}, lets look for an already maybe_car we might have'.format(car_box))
+            if len(cars_in_box) == 0:
+                # 2 things can happen now, due to the bad estimation of cars, maybe this box
+                # belongs to one of the cars but that car's bounding box is so much off
+                # that it says it does not belong to it
+                # So lets keep a set of all boxes that had no car and then match it with the set of 
+                # cars that say they have no bounding boxes
+
+                print('SVC/Label says there is a box: {}, lets look for an already maybe_car we might have'.format(car_box))
 
                 # Check in case we already have this bounding box in a maybe_car.
                 # Increment frame connt and see if it is to be moved to a car now
-                car = self.get_car_for_bbox(car_box, which_cars = self.maybe_cars)
+                maybe_cars_in_box = self.get_car_for_bbox(car_box, which_cars = self.maybe_cars)
 
-                if car is not None:
-                    print('Ok we found a maybe car, lets see if this one was recent or very old')
-
-                    no_of_frames_passed = self.last_frame_no - car.frame_no_when_seen 
-                    
-                    # in case the car we found in the maybe list is very old and is a zoombie
-                    # don't consider it the same car at all
-                    
-                    if no_of_frames_passed > MAX_FRAME_COUNT_MAYBE_CARS:
-                        print('An old maybe car, earlier seen at {} found, resurrecting it but still a maybe not actual'.format(car.frame_no_when_seen))
-
-                        car.frame_no_when_seen = self.last_frame_no
-                        car.frame_count = 1
-                        car.bounding_box = car_box
-                    else:
-                        print('Recent may_be, last seen {} frames ago. Total frames: {} Total Required: {} .'.format(no_of_frames_passed, car.frame_count, FRAME_COUNT_MAYBE_CARS))
-
-                        car.frame_count += 1
-                        
-                        # there should be at least 5  frames that the car is visible for and
-                        # it should be visible at least in 50% of the last frames
-                        if (car.frame_count > FRAME_COUNT_MAYBE_CARS) and (car.frame_count / no_of_frames_passed > 0.5):
-                            w,h = get_w_h(car_box)
-                            if (w > 20) and (h > 20):
-                                self.cars.append(car)
-                                cars_seen_this_frame[car] = True  # add a new car to the cars_seen array
-
-                                print('Remove from maybe cars and adding it to the active car list')
-                                self.maybe_cars.remove(car)
-                            else:
-                                print('Too small to be a car')
+                if len(maybe_cars_in_box) > 0:
+                    cars_moved = self.update_maybe_cars(maybe_cars_in_box, car_box)
+                    cars_seen_this_frame |= cars_moved
                 else:
-                    # check how close are we to another car, may be its a  false
-                    # bounding box from labels, we will add it to the possible cars array
-                    # and will see if it is still there after some frames or not
-
-                    if (len(self.cars) > 1) and (not self.very_close_to_other(car_box)):
-                        car = Vehicle(car_no)
-                        car.frame_no_when_seen = self.last_frame_no
-
-                        self.cars.append(car)
-                        cars_seen_this_frame[car] = True  # add a new car to the cars_seen array
-                    else:
-                        car = Vehicle(car_no)
-                        car.frame_no_when_seen = self.last_frame_no
-                        
-                        self.maybe_cars.append(car)
+                    car = self.add_car(car_box)
+                    cars_seen_this_frame.add(car)
+                    cars_in_box.add(car)
             else:
-                cars_seen_this_frame[car] = True
+                cars_seen_this_frame |= set(cars_in_box)
 
-            # may be it was a false prediction from labels and we are not going to add this car
-            if car is not None:
-                # maybe label is confused and giving us a rectangle that is either merged
-                # or very close to some other car, stick to our update DO NOT believe what
-                # label is saying
-                if (car.bounding_box is not None) and self.very_close_to_other(car_box):
-                    # how big or small is the new box compared to our old box
-                    cb_w, cb_h = get_w_h(car_box)
-                    car_w, car_h = get_w_h(car.bounding_box)
-
-                    delta_h = np.abs(car_w - cb_w)
-                    delta_w = np.abs(car_h - cb_h)
-
-                    if (delta_h / car_h < 0.5) and (delta_w / car_w < 0.5):
-                        car.update_box(car_box)
-                else:
-                    car.update_box(car_box)
+            self.update_car_estimates(cars_in_box, car_box)
             
-        # is there a car that we did not see this time? if yes, maybe
-        # it could have been overlapping
-        for car in cars_seen_this_frame:
-            if cars_seen_this_frame[car] == False:
-                print('Did not see car #', car.car_no, ' speed:', car.speed)
-                
-                # check if there were any boxes in which this car was detected?
-                car_windows = []
-                for window in windows:
-                    if car.is_box_enclosing(window):
-                        car_windows.append(window)
-
-                if len(car_windows) > 0:
-                    print('Looks like there were some windows')
-
-                    # make a local heatmap, find an enclosing box around it and then
-                    # use that to update the car bounding box
-                    heatmap = np.zeros(shape=self.img_cs.shape[:2])
-                    self.car_finder.add_heat(car_windows, heatmap)
-
-                    heatmap /= np.max(heatmap)
-                    heatmap[heatmap < 0.7] = 0
-                    zy, zx = heatmap.nonzero()
-                    left = np.min(zx)
-                    right = np.max(zx)
-                    top = np.min(zy)
-                    bottom = np.max(zy)
-
-                    #bbox = self.make_bounding_box(car_windows)
-                    bbox = ((left, top), (right,bottom))
-                    car.update_box(bbox)
-                    cars_seen_this_frame[car] = True
-                else:
-                    cars_seen_this_frame[car] = self.car_gone_out(car)
-
-        # remove all cars that were not seen
-        for index, car in enumerate(self.cars):
-            if not cars_seen_this_frame[car]:
-                self.cars_gone_off.append(car)
-                del self.cars[index]
+        self.check_cars_gone_off(cars_seen_this_frame, windows)
 
         # # check maybe cars might need to be kicked out now
         # if len(self.maybe_cars) > 0:
@@ -479,6 +393,114 @@ class VehicleIdentifier():
         #     for car in copy_maybe_cars:
         #         if self.last_frame_no - (car.frame_no_when_seen + car.frame_count) < FRAME_COUNT_MAYBE_CARS:
         #             self.maybe_cars.append(car)
+
+
+    def check_cars_gone_off(self, cars_seen_this_frame, windows):
+        missing_cars = self.cars - cars_seen_this_frame
+
+        for car in missing_cars:
+            print('Did not see car #: {} estimated speed {}'.format(car.car_no,car.speed))
+            if not self.ok_to_remove_gone_off(car):
+                self.cars.add(car)
+            
+            # Maybe a False Negative
+            # Our heatmap thresholded this car, but there was an actual detection?
+            # if yes, then we need to update this based on our detection
+
+            # check if there were any boxes in which this car was detected?
+            # car_windows = []
+            # for window in windows:
+            #     if car.is_box_enclosing(window):
+            #         car_windows.append(window)
+
+            # if len(car_windows) > 0:
+            #     print('Looks like there were some windows')
+
+            #     # make a local heatmap, find an enclosing box around it and then
+            #     # use that to update the car bounding box
+            #     heatmap = np.zeros(shape=self.img_cs.shape[:2])
+            #     self.car_finder.add_heat(car_windows, heatmap)
+
+            #     heatmap /= np.max(heatmap)
+            #     heatmap[heatmap < 0.5] = 0
+            #     nonzeroy, nonzerox = heatmap.nonzero()
+            #     bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+
+            #     car.update_box(bbox)
+            #     cars_seen_this_frame[car] = True
+            # else:
+            #     self.car_gone_out(car)
+
+    def add_car(self, car_box):
+        car_no = len(self.cars) + 1
+
+        car = Vehicle(car_no)
+        car.frame_no_when_seen = self.last_frame_no
+        
+        # check how close are we to another car, may be its a  false
+        # bounding box from labels, we will add it to the possible cars array
+        # and will see if it is still there after some frames or not
+
+        if (len(self.cars) > 0) and (not self.very_close_to_other(car_box)):
+            self.cars.add(car)
+        else:
+            self.maybe_cars.add(car)
+
+        return car
+
+    # returns cars, which have been moved from maybe to actual cars
+    def update_maybe_cars(self, cars_in_box, car_box):
+        cars_moved = set()
+        print('Ok we found {} maybe cars, lets see if this one was recent or very old'.format(len(cars_in_box)))
+
+        for car in cars_in_box:
+            no_of_frames_passed = self.last_frame_no - car.frame_no_when_seen 
+            
+            # in case the car we found in the maybe list is very old and is a zoombie
+            # don't consider it the same car at all
+            
+            if no_of_frames_passed > MAX_FRAME_COUNT_MAYBE_CARS:
+                print('An old maybe car, earlier seen at {} found, resurrecting it but still a maybe not actual'.format(car.frame_no_when_seen))
+
+                car.frame_no_when_seen = self.last_frame_no
+                car.frame_count = 1
+                car.bounding_box = car_box
+            else:
+                print('Recent may_be, last seen {} frames ago. Total frames: {} Total Required: {} .'.format(no_of_frames_passed, car.frame_count, FRAME_COUNT_MAYBE_CARS))
+
+                car.frame_count += 1
+                
+                # there should be at least 5  frames that the car is visible for and
+                # it should be visible at least in 50% of the last frames
+                if (car.frame_count > FRAME_COUNT_MAYBE_CARS) and (car.frame_count / no_of_frames_passed > 0.5):
+                    w,h = get_w_h(car_box)
+                    if (w > 20) and (h > 20):
+                        self.cars.add(car)
+                        cars_moved.add(car)
+                        print('Remove from maybe cars and adding it to the active car list')
+                        self.maybe_cars.remove(car)
+                    else:
+                        print('Too small to be a car')
+
+        return cars_moved
+
+    def update_car_estimates(self, cars_found, car_box):
+        for car in cars_found:
+            # maybe label is confused and giving us a rectangle that is either merged
+            # or very close to some other car, stick to our update DO NOT believe what
+            # label is saying
+            if (car.bounding_box is not None) and self.very_close_to_other(car_box):
+                # how big or small is the new box compared to our old box
+                cb_w, cb_h = get_w_h(car_box)
+                car_w, car_h = get_w_h(car.bounding_box)
+
+                delta_h = np.abs(car_w - cb_w)
+                delta_w = np.abs(car_h - cb_h)
+
+                if (delta_h / car_h < 0.5) and (delta_w / car_w < 0.5):
+                    car.update_box(car_box)
+            else:
+                car.update_box(car_box)
 
     def very_close_to_other(self, box):
         very_close = False
@@ -539,20 +561,20 @@ class VehicleIdentifier():
     # a car that used to be in the frame is no longer with us, lets
     # try to create a new window that is centered at the last point we 
     # knew for the car and then see may be it is still there
-    def car_gone_out(self, car):
+    def ok_to_remove_gone_off(self, car):
         car.miss_frames += 1
 
-        if car.miss_frames > MAX_MISSFRAMES:
+        if car.miss_frames < MAX_MISSFRAMES:
             return False
 
         # we did estiamte that the car would be going out so its ok if it is no longer
         # in frame
         if car.might_go_out():
-            return False
+            return True
 
         # the car should have been with us for long before we do a center search on it
-        if car.frame_count < MIN_FRAMES_TO_CENTER_SEARCH:
-            return False
+        # if car.frame_count < MIN_FRAMES_TO_CENTER_SEARCH:
+        #     return False
 
         print('Car gone out, it has been with us for {} frames'.format(car.frame_count))
 
@@ -588,8 +610,11 @@ class VehicleIdentifier():
             car_seen = True
 
             self.add_windows = car_windows
+        else:
+            self.cars_gone_off.add(car)
+            self.cars.remove(car)
 
-        return car_seen
+        return not car_seen
 
     def overlay_images(self):
         output_img = np.copy(self.img_rgb)
@@ -696,8 +721,8 @@ if __name__ == '__main__':
         identifier = VehicleIdentifier(params)
         print('New identifier created')
 
-
-    for identifier.last_frame_no in range(identifier.last_frame_no + 1, identifier.last_frame_no + 400):
+    #for identifier.last_frame_no in range(identifier.last_frame_no + 1, identifier.last_frame_no + 400):
+    for identifier.last_frame_no in range(identifier.last_frame_no, 1280):
         #filename = './project_video-frames/{:04d}.jpg'.format(identifier.last_frame_no)
         filename, window_filename = get_frame_filenames(identifier.last_frame_no)
         
@@ -714,6 +739,7 @@ if __name__ == '__main__':
                 window_features = window_data['features']
 
         output_img = identifier.process_frame(img, car_windows = car_windows, window_features = window_features)
+
         t2 = time.time()
         print('Frame {}, time taken {:.3f} secs'.format(identifier.last_frame_no, t2 - t1))
 
